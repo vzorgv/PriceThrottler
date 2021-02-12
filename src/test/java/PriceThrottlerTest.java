04e37125efe4c2ff33ad8358d96893f21a7ceef5
Dmitry Pereverzev <vzorgv@gmail.com>
2021-02-12 19:53:30 +0300

    Transfer pair of requirements tp testable artifacts

---
+++ src/test/java/PriceThrottlerTest.java
@@ -0,0 +1,18 @@
+import org.junit.jupiter.api.DisplayName;
+import org.junit.jupiter.api.Test;
+
+public class PriceThrottlerTest {
+
+    @Test
+    @DisplayName("When event comes then distributed to listeners")
+    public void eventShouldBeDistributedToListeners() {
+
+    }
+
+
+    @Test
+    @DisplayName("ONLY LAST PRICE for each ccyPair matters for subscribers.")
+    public void theLastPriceShouldBeProcessed() {
+
+    }
+}
